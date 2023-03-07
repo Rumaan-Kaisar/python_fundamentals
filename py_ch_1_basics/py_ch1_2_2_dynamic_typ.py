80e2977fc8c6c9c7cc7cadf56789aea4a0ecb4d4
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2023-03-07 13:10:00 +0600

    Add files via upload

---
+++ py_ch_1_basics/py_ch1_2_2_dynamic_typ.py
@@ -0,0 +1,21 @@
+# Courses: colt_py_bootcamps 46
+
+# ------------ Dynamically type --------------
+""" 
+    Reassign variables to different types.
+    In Python the data-type of variable is not fixed, we dont need to declare the data type
+    The data-type of a variable changes if we give it to different type of values
+    Opposite of C++ and java (these are 'statically typed') 
+"""
+
+awesomeness = True
+print(awesomeness)  # prints True
+
+awesomeness = "La La LA"
+print(awesomeness)  # prints La La LA
+
+awesomeness = 3.14159
+print(awesomeness)  # prints 3.14159
+
+awesomeness = None
+print(awesomeness)  # prints None
