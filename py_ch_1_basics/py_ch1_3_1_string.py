be650a84ec7e646d1ba24241472c73590197ff10
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2023-03-21 21:31:02 +0600

    Update py_ch1_3_1_string.py

--- py_ch_1_basics/py_ch1_3_1_string.py
+++ py_ch_1_basics/py_ch1_3_1_string.py
@@ -11,3 +11,13 @@ str_2 = "i am a cat, inside a hat"
 str_3 = 'she says "my name is Pamanela"'
 str_4 = "he said : 'my name is Robin'"
 print(str_1+'\n'+str_2+'\n'+str_3+'\n'+str_4+'\n')
+
+
+# ---------|    String Multiplication    |---------
+# Notice: "String" is multiplied by a "number"
+# Multiplication is adding multiple times
+
+times = 1
+while times < 11:
+	print("\U0001f600" * times)
+	times += 1
