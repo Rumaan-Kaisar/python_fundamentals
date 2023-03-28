477b968abff42f95bf26cdaa41fd3e35c230420a
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2023-03-28 14:49:42 +0600

    Update py_ch2_1_1_list_intro.py

--- py_ch_2_countables/py_ch2_1_1_list_intro.py
+++ py_ch_2_countables/py_ch2_1_1_list_intro.py
@@ -1,4 +1,4 @@
-# Courses: colt_py_bootcamps    97
+# Courses: colt_py_bootcamps    97, 98, 100
 
 """ 
     Lists are one of 4 built-in data types in Python used to store collections of data, 
@@ -11,6 +11,47 @@
     # Changable
     # allow duplicate values
 
+# simple list
+thislist = ["apple", "banana", "cherry", "apple", "banana", "cherry", "apple", "cherry"]
+print(thislist)
+
+# List items can be of any data type, it can contain different data types
+list1 = ["abc", 34, True, 40, "male"]
+
+# list() Constructor: It is also possible to use the list() constructor when creating a new list.
+thislist = list(("apple", "banana", "cherry")) # note the double round-brackets
+print(thislist)
+
+
+# ------------|    List Length    |------------
+# To determine how many items a list has, use the len() function
+thislist = ["apple", "banana", "cherry"]
+print(len(thislist))
+
+
+# Converting/Turning a 'sequence' into a 'list'
+r = range(1, 11)
+r_list = list(r)
+print(r_list)
 
 
 
+
+#  ------------|    Accessing list items    |------------
+""" 
+        -ve values to count from last item.
+            0       1           2
+        ["apple", "banana", "cherry"]
+            -3      -2          -1 
+"""
+thislist = ["apple", "banana", "cherry"]
+print(len(thislist[2])) # cherry
+print(len(thislist[-1])) # cherry
+
+# Check a value in a list
+"apple" in thislist     # true
+"mango" in thislist     # false
+print(f"{'apple' in thislist} and {'mango' in thislist}")
+# so we can use it as a condition:
+if "apple" in thislist:
+    print("apple is on us")
