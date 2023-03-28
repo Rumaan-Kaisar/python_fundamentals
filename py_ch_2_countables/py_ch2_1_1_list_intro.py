17dfe47f9402a622d859996f6f5e801a66504679
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2023-03-28 14:21:23 +0600

    Add files via upload

---
+++ py_ch_2_countables/py_ch2_1_1_list_intro.py
@@ -0,0 +1,16 @@
+# Courses: colt_py_bootcamps    97
+
+""" 
+    Lists are one of 4 built-in data types in Python used to store collections of data, 
+        the other 3 are Tuple, Set, and Dictionary, all with different qualities and usage. 
+"""
+
+# Lists are created using square brackets []
+    # List are :
+    # Ordered
+    # Changable
+    # allow duplicate values
+
+
+
+
