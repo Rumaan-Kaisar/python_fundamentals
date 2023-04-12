0eb62e3bfee12cca1a22eb83a922d446f10ce2f4
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2023-04-12 12:06:56 +0600

    Update py_ch2_3_1_dict.py

--- py_ch_2_countables/py_ch2_3_1_dict.py
+++ py_ch_2_countables/py_ch2_3_1_dict.py
@@ -91,3 +91,18 @@ full_name = "{} {}".format(artist["first"], artist["last"])
 full_name = f"{artist['first']} {artist['last']}"
 
 
+
+# Example 3: Loop over 'donations', add all the VALUES together and store in a variable called "total_donations"
+
+donations = dict(sam=25.0, lena=88.99, chuck=13.0, linus=99.5, stan=150.0, lisa=50.25, harrison=10.0)
+total_donations = 0
+for donation in donations.values():
+    total_donations += donation
+
+# Advanced Version 1: This solution uses a built-in function called sum() covered in the "Lambdas and Built-In Functions" section.
+total_donations = sum(dntn for dntn in donations.values())
+
+# Advanced Version 2: An even better solution using the same sum() built-in function is just this nice little line:
+sum(donations.values())
+
+
