52e03a0f88d13528af26b4bac2b30d1f193f9d82
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2023-04-18 14:53:22 +0600

    Add files via upload

---
+++ py_ch_4_functions/py_ch4_1_1_function.py
@@ -0,0 +1,63 @@
+
+# Courses: colt_py_bootcamps    145, 146, 147
+
+# -------|    OBJECTIVES    |-------
+    # Describe what a function is and how they are useful
+    # Explain exactly what the "return" keyword does and some of the side effects when using it
+    # Add "parameters" to functions to output different data
+    # Define and diagram how "scope" works in a function
+    # Add "keyword arguments" to functions
+
+
+# What is a Function?
+    # A process for executing a task
+    # It can accept input and return an output
+    # Useful for executing similar procedures over and over
+
+
+# Why Use Functions?
+    # Stay "DRY" - Don't Repeat Yourself!
+    # Clean up and prevent code duplication 
+    # "Abstract away" code for other users 
+        # Imagine if you had to rewrite the "print()" function for every program you wrote
+
+
+
+# -------|    Function Structure    |-------
+def name_of_function(params):
+    # Code Block to execute
+
+# Execute: To execute a function always call the function with "()"
+name_of_function(args)
+
+
+
+# Example 1: Define and execute a function
+def say_my_name():
+    print(f"You're Heisenberg!!")
+
+say_my_name()   # You're Heisenberg!!
+
+
+
+# Example 2: Another simple function
+def sing_happy_birthday():
+    print("Happy Birthday To You") 
+    print("Happy Birthday To You") 
+    print("Happy Birthday Dear 'Blah! Blah Blah!'") 
+    print("Happy Birthday To You")
+
+sing_happy_birthday()
+
+
+# Example 3: Define make_noise with parameters
+def make_noise(nm_prm):
+    print(f"THE {nm_prm} GOES WILD")
+
+# Execute make_noise:
+make_noise("GAGNAM")
+
+
+
+
+
