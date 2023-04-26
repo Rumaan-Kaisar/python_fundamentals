78a0915d74ed50cee159e867dd06c90212a1fbd3
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2023-04-26 14:04:39 +0600

    Update py_ch4_2_1_func_param.py

--- py_ch_4_functions/py_ch4_2_1_func_param.py
+++ py_ch_4_functions/py_ch4_2_1_func_param.py
@@ -26,7 +26,7 @@ def print_full_name(first_name, last_name):
 
 print_full_name("Andy", "Volok")
 
-# parameters & arguments
+# ---------------    parameters & arguments    ---------------
 # in above code first_name, last_name are "parameters" but "Andy", "Volok" are arguments
 
 
@@ -51,3 +51,30 @@ def yell(word):
     return f"{word.upper()}!"
 
 yell("bon bon")
+
+
+
+
+# Example 5: Keep track of the days in a list.  
+    # Check to make sure num isn't < 0 or  > 6.  
+    # Return the corresponding day. Use 'days[num-1]' because 'return_day(1)' should return 0th item in list.
+
+def return_day(num):
+    days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    # Check to see if num valid
+    if num > 0 and num <= len(days):
+        # use num - 1 because lists start at 0 
+        return days[num-1]
+    return None
+
+# BONUS ADVANCED VERSION:
+    # Here's a more advanced solution that involves error handling, which we have not covered yet.  
+    # It eliminates the need to check to see if num is a 'valid index' in the list.
+
+def return_day(num):
+    try:
+        return ["Sunday","Monday", "Tuesday","Wednesday","Thursday","Friday","Saturday"][num-1]
+    except IndexError as e:
+        return None
+
+    
