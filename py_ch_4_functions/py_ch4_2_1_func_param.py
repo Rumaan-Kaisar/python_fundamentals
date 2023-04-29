84da9d641b1565a3254c00e7e523792e6f4d9cb9
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2023-04-29 12:45:46 +0600

    Update py_ch4_2_1_func_param.py

--- py_ch_4_functions/py_ch4_2_1_func_param.py
+++ py_ch_4_functions/py_ch4_2_1_func_param.py
@@ -175,3 +175,75 @@ def compact(l):
     for val in l:
         if val: truthy_vals.append(val)
     return truthy_vals
+
+
+
+
+# Example 12 : Define a function that finds common elements in two given lists
+    # Manual Looping Solution
+        # Here's one potential solution:
+            # Define an empty list that will eventually store the in common values
+            # Loop through one list (l1)
+            # For each value, check if that value is in the other list (l2)
+            # If it is, append the value to the in_common list
+            # Return in_common  after the loop ends
+
+def intersection(l1, l2):
+    in_common = []
+    for val in l1:
+        if val in l2:
+            in_common.append(val)
+    return in_common
+
+
+# List Comprehension Solution
+    # The first solution is perfectly valid.  It's a naive way of solving the problem.  
+    # A more Python-ic solution involves using a "list comprehension" to do the same thing on a single line. 
+
+def intersection(l1, l2):
+    return [val for val in l1 if val in l2]
+
+
+# Sets Solution: Sets Solution is the most efficient of the three.  
+    # It converts the lists to sets, which removes duplicate values, and then finds the intersection of them using &.  
+
+def intersection(list1, list2):
+    return [val for val in set(list1) & set(list2)]
+
+
+
+
+# Eaxmple 13: Seperate values from a  list according to a given function, satisfies or not.
+
+    # "Traditional" Version
+        # Here's my solution that doesn't use a list comprehension:
+
+        # I have two lists, to hold the true and false values
+        # I loop through the list, calling fn  with each value in the list
+        # If the result is True, I append the value to the trues  list
+        # Otherwise, I append the value to the falses  list
+        # At the end I return a list that contains both the trues  and falses  lists
+def partition(lst, fn):
+    trues = []
+    falses = []
+    for val in lst:
+        if fn(val):
+            trues.append(val)
+        else:
+            falses.append(val)
+    return [trues, falses]
+
+
+# List Comprehension Version
+    # Using a list comprehension, you can get this function down to a single line.  
+    # It's definitely a tradeoff though.  It's much short but also more difficult to understand.  
+    # It's a fine balance between brevity and readability.
+
+def partition(lst, fn):
+    return [[val for val in lst if fn(val)], [val for val in lst if not fn(val)]]
+
+
+# Another Solution using callback function
+def partition(l, callback):
+    return [[l.pop(l.index(i)) for i in l if callback(i)],l]
+
