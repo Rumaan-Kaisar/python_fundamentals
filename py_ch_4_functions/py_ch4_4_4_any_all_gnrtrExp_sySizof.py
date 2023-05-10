ab3392789c177c468ca03905627824a34c263688
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2023-05-10 12:00:13 +0600

    Update py_ch4_4_4_any_all_gnrtrExp_sySizof.py

--- py_ch_4_functions/py_ch4_4_4_any_all_gnrtrExp_sySizof.py
+++ py_ch_4_functions/py_ch4_4_4_any_all_gnrtrExp_sySizof.py
@@ -56,3 +56,28 @@ any([num%2==1 for num in numsAny]) # True
 
 
 # --------------    Generator Expression    --------------
+# No need to use list comprehension braces
+numsAny = [1, 4, 2, 10, 6, 8]
+# [num for num in numsAny if num%2==0]
+any(num for num in numsAny if num%2==0)     # true
+
+# Generator Expression
+(num for num in numsAny if num%2==0)    # <generator object <genexpr> at 0x000001C6A4C49A10>
+# 'generator object' is not a list, 
+    # "LISTS" are havier than "generator object"
+    # To save memory/space we'll use 'generator object' instead of list comprehension
+
+# Basically, use a "generator expression" if all you're doing is "iterating once". 
+# If you want to store and use the generated results, then you're probably better off with a list comprehension.
+
+
+# Example 4: Memory usage Demo:
+import sys
+list_comp = sys.getsizeof([x*10 for x in range(1000)]) 
+gen_exp = sys.getsizeof(x*10 for x in range(1000))
+
+print('To do the same thing, it takes...') 
+print( f'List Comprehension: {list_comp} bytes') # List Comprehension: 8856 bytes
+print(f"Generator Expression: {gen_exp} bytes") # Generator Expression: 104 bytes
+
+
