af57349857d71c0eced0ea84c907a6b601ca38fa
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2023-05-13 12:42:36 +0600

    Update py_ch4_4_7_len()_oop_pVw.py

--- py_ch_4_functions/py_ch4_4_7_len()_oop_pVw.py
+++ py_ch_4_functions/py_ch4_4_7_len()_oop_pVw.py
@@ -1,12 +1,37 @@
-
-# Courses: colt_py_bootcamps    197
-
-# -----------------    len()    -----------------
+# Courses: colt_py_bootcamps    197, 
 
 
+# -------------------    len()    -------------------
+# Returns length (number of items) of an object
+# argument may be sequence (string, tuple, list or range) or a collection (dictionary, set)
 
+len("awesome")      # string
+len([1,2,3,4,5,6])  # list
+len((1,2,3,4,5,6))  # tuple
+len(range(0, 10))   # range
+
+len({1,2,3,4,5,6,7})    # set
+len({"a":1, "b":2, "c":3})    # dictionary
+
+
+
+
+# -------------    how its work?    --------------
+# there is a __len__() method for all sequence and collection 
+    # i.e. a string, list, tuple or dictionary already has this __len__() by their creation
+    # behind the scene 'len()' is actually calling '_len_()'
+"hello".__len__()   # 5, works the same way as len()
+
+
+
+
+# -------------    User defined __len__()    --------------
+# In OOP we can define our own version of __len__(), and we can call len() on our object
+
+
+# Example 1: notice in the following class definition we are creating __len__()
 class SpecialList:
-     
+ 
     def __init__(self, data):
         self.__data = data
  
@@ -21,4 +46,4 @@ print(len(l1)) #50
 print(len(l2)) #50
 
 
-
+ 
