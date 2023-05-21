2dd959269a53ef3bf53da0e084dce8e806126cc3
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2023-05-21 12:23:29 +0600

    Add files via upload

---
+++ py_ch_6_modules_packages/py_ch6_1_1_modules_builtn.py
@@ -0,0 +1,22 @@
+
+# Courses: colt_py_bootcamps    214
+
+# There are different pre-build modules for different works:
+
+    # Color document
+    # Add ascii art
+    # HTTP request
+    # connect to a database
+
+''' 
+    ----------    OBJECTIVES    ----------
+    •	Define what a module is
+    •	Import code from built-in modules
+    •	Import code from other files
+    •	Import code from external modules using "pip"
+    •	Describe common module patterns
+    •	Describe the request/response cycle in "HTTP"
+    •	Use the "requests" module to make requests to web apps
+'''
+
+----------    OBJECTIVES    ----------
