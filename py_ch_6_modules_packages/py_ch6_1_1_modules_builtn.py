5daa8ac1f4cdf6e2aeba1a690de7f898576f923e
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2023-05-21 13:02:05 +0600

    Update py_ch6_1_1_modules_builtn.py

--- py_ch_6_modules_packages/py_ch6_1_1_modules_builtn.py
+++ py_ch_6_modules_packages/py_ch6_1_1_modules_builtn.py
@@ -1,5 +1,5 @@
 
-# Courses: colt_py_bootcamps    214
+# Courses: colt_py_bootcamps    214, 215
 
 # There are different pre-build modules for different works:
 
@@ -19,4 +19,69 @@
     •	Use the "requests" module to make requests to web apps
 '''
 
-----------    OBJECTIVES    ----------
+# ----------    Why Use Modules?    ----------
+    # Keep Python files small - DRY code
+    # To manage you code better
+    # Reuse code across multiple files by importing 
+    # A module is just a Python file!
+
+
+
+
+# ----------    Built-in Modules    ----------
+# Built-in Modules Example
+import random			
+random.choice(["apple",	"banana",	"cherry", "durian"])
+random.randint(1, 100)
+random.shuffle(["apple",	"banana",	"cherry", "durian"])
+
+# import module by giving it a different name
+import random as oMg_rand
+frt_lst = ["apple",	"banana",	"cherry", "durian"]
+print(oMg_rand.choice(frt_lst))
+oMg_rand.randint(1, 100)
+print(oMg_rand.shuffle(frt_lst))
+
+''' -------- CLI --------
+>>> oMg_rand.shuffle(frt_lst)
+>>> frt_lst
+['banana', 'durian', 'cherry', 'apple']
+>>> oMg_rand.shuffle(frt_lst)
+>>> frt_lst
+['durian', 'banana', 'apple', 'cherry'] 
+'''
+
+
+
+# ----------    Importing Parts of a Module    ----------
+# Importing Parts of a Module
+    # The "from" keyword lets you import parts of a module
+
+from random import randint, choice
+choice(frt_lst)
+randint(1, 100)
+
+# Importing Parts of a Module by giving different name
+from random import randint as alpha, choice as beta
+beta(frt_lst)
+alpha(1, 100)
+
+# Importing all Parts of a Module
+from random import *
+
+# Note:
+    # From my point of view, importing parts from a module can lead to confusioin
+    # Because different module can contain parts that can have same NameError
+    # Resulting "Name Collision"
+    # Better choice is: "module.property"
+
+
+
+
+# ----------    Different Ways to Import    ----------
+# All of these work!
+    # •	import random
+    # •	import random as omg_so_random
+    # • from random import *
+    # • from random import choice, shuffle
+    # •	from random import choice as gimme_one, shuffle as mix_up_fruits
