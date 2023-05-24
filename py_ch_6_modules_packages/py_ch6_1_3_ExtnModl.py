3d1c730d45ec3978bf2acbd17a2747db0ca19ac4
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2023-05-24 15:33:47 +0600

    Update py_ch6_1_3_ExtnModl.py

--- py_ch_6_modules_packages/py_ch6_1_3_ExtnModl.py
+++ py_ch_6_modules_packages/py_ch6_1_3_ExtnModl.py
@@ -1,2 +1,64 @@
 
-# Courses: colt_py_bootcamps 
+# Courses: colt_py_bootcamps 220
+
+# ---------    External Modules    ---------
+    # Built-in modules come with Python
+    # External modules are downloaded from the internet
+    # You can download external modules using "pip"
+
+# pypi: Collection of External Modules
+    # Browse: Find more packages at https://pypi.org/.
+        # Graphics
+        # AUDIO
+        # nlp 
+        # Sound to speech
+
+# ---------    pip    ---------
+''' Package management system for Python 
+As of 3.4, comes with Python by default
+
+General Syntax:
+
+    python3 -m pip install NAME_OF_PACKAGE
+or
+    pip install NAME_OF_PACKAGE '''
+
+
+# How to get and install pip:
+    # If you do not have PIP installed, you can download and install it from this page: https://pypi.org/project/pip/
+
+
+
+# check if pip is installed/PIP version:
+#     pip --version
+
+
+
+
+# Example 1: Installing packages for a Machine Learning Environment
+pip install scikit-learn
+pip install numpy
+pip install matplotlib
+pip install statsmodels
+
+# install as admin
+pip install --user tensorflow
+
+# ---------    colorize print: termcolor    ---------
+
+
+
+
+# Remove a Package:
+    # Use the "uninstall" command to remove a package:
+
+# Example 2: Uninstall the package named "camelcase":
+pip uninstall camelcase
+
+
+
+
+# List Packages
+    # Use the list command to list all the packages installed on your system:
+pip list
+
