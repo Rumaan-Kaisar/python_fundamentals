4a518910ce23e5a0f88ce2b3bcc70b1fa2766d86
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2023-05-31 12:16:33 +0600

    Update py_ch12_1_2_Verbs_APIs.py

---
+++ py_ch_12_webScrp/py_ch12_1_2_Verbs_APIs.py
@@ -0,0 +1,36 @@
+
+
+# Courses: colt_py_bootcamps    226
+
+# ---------------    GET    ---------------
+    # Useful for retriving data
+    # Data passed in query string
+    # Should have no "side effect" (content of the server wont change)
+    # Can be cached
+    # Can be bookmarked
+
+
+
+
+# ---------------    POST    ---------------
+    # Useful for writing data
+    # Data passed in request body
+    # can have "side effect" (content of the server updates)
+        # EG: uploading image/videos, giving email/password
+    # Not cached
+    # Can't be bookmarked
+
+
+
+
+# ---------------    API    ---------------    
+# API - Application Programming Interface
+    # Its a interface to get/send data from/to serevr
+    # allows you to get data from another application withot understanding how that application works
+    # can often send data back in different formats
+    # Example of companies with APIs: GitHub, Spotify, Google
+
+# Most of the time we use "GET" request, just getting information from API
+    # Data is in the XML or JSON format
+    # For example: "weather.com" has API, we can use that API to get weather info (in JSON format)
+        # common formats are "api.weather.com" or "weather.com/api"
