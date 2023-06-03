e33822d0b992e2a0d2fd1e0c13c5fd0b1adbb53f
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2023-06-03 12:30:34 +0600

    Add files via upload

---
+++ py_ch_12_webScrp/py_ch12_1_6_api_pjct.py
@@ -0,0 +1,42 @@
+
+# Courses: colt_py_bootcamps    230
+
+# ----------------    API Project    ----------------
+
+import requests
+import os	
+os.system('color')
+import pyfiglet
+import termcolor
+from random import choice
+
+header = pyfiglet.figlet_format("Dad Joke 3000")
+header = termcolor.colored(header, color="magenta")
+
+print(header)
+
+term = input("Let me tell you a joke! Give me a topic: ")
+
+response_json = requests.get(
+    "https://icanhazdadjoke.com/search",
+    headers={"Accept": "application/json"},
+    params={"term": term}
+).json()
+
+results = response_json["results"]
+total_jokes = response_json["total_jokes"]
+
+if total_jokes > 1:
+    print(
+        f"I've got {total_jokes} jokes about {term}. Here's one:\n",
+        choice(results)['joke']
+    )
+elif total_jokes == 1:
+    print(
+        f"I've got one joke about {term}. Here it is:\n",
+        results[0]['joke']
+    )
+else:
+    print(f"Sorry, I don't have any jokes about {term}! Please try again.")
+
+# python py_ch12_1_6_api_pjct.py
