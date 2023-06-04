7dd4e5ba75251cbbf90f8174a007faaf4ea3db56
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2023-06-04 10:46:28 +0600

    Update py_ch5_1_1_OOP_intro.py

--- py_ch_5_OOP/py_ch5_1_1_OOP_intro.py
+++ py_ch_5_OOP/py_ch5_1_1_OOP_intro.py
@@ -1,5 +1,5 @@
 
-# Courses: colt_py_bootcamps    232
+# Courses: colt_py_bootcamps    232, 233
 
 # Objectives:
     # Define OOP
@@ -7,4 +7,19 @@
     # Create CLASS & INSTANCES and attach methods and properties to them
 
 
-    
+# ------------    OOP    ------------    
+    # OOP is a method to model some process or thing in the real world as class or object
+
+# ---------    class & instances    ---------
+# Class
+    # bluprient - contains METHODS (functions) and ATTRIBUTES (similar to keys in dict)
+
+# instance
+    # objects that are constructed from a class that contains their class's method and properties
+
+
+# int, list, all those are actually "objects"
+    # use "help()" to retrive documentation of Python
+        # help() provides all sort of infromation including all "class-methods"
+    # for example: "list" is a class and "num = [1, 2, 3]" is a INSTANCE of the 'list-class'
+
