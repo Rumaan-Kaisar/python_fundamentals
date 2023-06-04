cb1d2273a3124391650331fb57b43d25b6ac6155
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2023-06-04 10:25:27 +0600

    Add files via upload

---
+++ py_ch_5_OOP/py_ch5_1_1_OOP_intro.py
@@ -0,0 +1,10 @@
+
+# Courses: colt_py_bootcamps    232
+
+# Objectives:
+    # Define OOP
+    # Understand ENCAPSULATION & POLYMORPHISM
+    # Create CLASS & INSTANCES and attach methods and properties to them
+
+
+    
