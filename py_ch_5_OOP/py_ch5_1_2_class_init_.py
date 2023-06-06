514fa7ac1387ff32204a60296f9495e15737188f
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2023-06-06 13:15:06 +0600

    Add files via upload

---
+++ py_ch_5_OOP/py_ch5_1_2_class_init_.py
@@ -0,0 +1,27 @@
+# Courses: colt_py_bootcamps    235
+
+
+# ------------------    class definition    ------------------
+# Convention: Always use "CamelCase" first letter in uppercase
+# use snake_case for "variables" and "functions"
+
+class Person:
+	pass    # Empty class are not valid in python, so we used "pass"
+
+p = Person()    # use () to create an 'Instance' of a class
+print(type(p))
+print(p)
+
+
+
+
+# Example 1: Define a class called Vehicle.  It should be completely empty (just add a pass statement inside) . 
+                # After the class is defined, create two instances of Vehicle.  
+                    # Save one to a variable called car  
+                    # Save another to a variable called boat.
+
+class Vehicle:
+    pass
+
+car = Vehicle()
+boat = Vehicle()
