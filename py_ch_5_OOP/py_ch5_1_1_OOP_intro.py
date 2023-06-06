45138a0beea4dbb6ec3d59db5aee5a0bd8bc273a
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2023-06-06 13:14:24 +0600

    Update py_ch5_1_1_OOP_intro.py

--- py_ch_5_OOP/py_ch5_1_1_OOP_intro.py
+++ py_ch_5_OOP/py_ch5_1_1_OOP_intro.py
@@ -32,3 +32,55 @@
     # get rid of SPAGETTI CODE
     
 
+
+
+# ---------    Example
+# we want to model a game of poker in our program. We could have the following entities:
+    # Game
+    # Player
+    # Card
+    # Deck
+    # Hand
+    # Chip
+    # Bet
+
+    # then we can have the following methods/properties to build a "Deck - class"
+""" 
+                Deck {class}
+
+        Card Deck Possible Implementation (Pseudocode)
+        _cards          {private list attribute} 
+        _max_cards      {private int attribute} 
+        shuffle         {public method}
+        deal_card       {public method}
+        deal_hand       {public method}
+        count           {public method}
+
+ """
+
+# Note about "Private" & "Public" 
+    # Actually there is no such thing in Python, its just a covention to follow
+    # "_name" means 'treat it as a private'
+
+
+
+# ---------    Encapsulation    ---------
+# Encapsulation - the grouping of 'public' and 'private' attributes and methods into a programmatic class, making abstraction possible
+    # Example
+        # Designing the Deck class, I make 'cards' a private attribute (a list)
+        # I decide that the 'length of the cards' should be accessed via a 'public' method called" count()" -- i.e. "Deck.count()"
+
+
+
+# ---------    Abstraction    ---------
+# Abstraction is used to hide the 'internal functionality' of the function from the users. 
+    # The users only interact with the basic implementation of the function, but inner working is hidden. 
+    # User is familiar with that "what function does" but they don't know "how it does."
+
+    # In simple words, we all use the smartphone and very much familiar with its functions such as camera, voice-recorder, call-dialing, etc., 
+    # but we don't know how these operations are happening in the background.
+
+
+
+# sometimes in OOP, we have to write more codes, but the code becomes more managable
+
