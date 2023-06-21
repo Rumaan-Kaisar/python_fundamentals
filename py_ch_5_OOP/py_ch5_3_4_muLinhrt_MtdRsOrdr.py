6d9c4f1147d18a39e755885d4628318a13d39ada
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2023-06-21 12:24:55 +0600

    Update py_ch5_3_4_muLinhrt_MtdRsOrdr.py

--- py_ch_5_OOP/py_ch5_3_4_muLinhrt_MtdRsOrdr.py
+++ py_ch_5_OOP/py_ch5_3_4_muLinhrt_MtdRsOrdr.py
@@ -3,6 +3,9 @@
 
 # ------------    Multiple inheritance    ------------
 
+from xml.dom import HierarchyRequestErr
+
+
 class Aquatic:
   def __init__(self,name):
     print("AQUATIC INIT!")
@@ -67,9 +70,6 @@ print(f"captain_cook is instance of Aquatic: {isinstance(captain_cook, Aquatic)}
 print(f"captain_cook is instance of Ambulatory: {isinstance(captain_cook, Ambulatory)}")
 
 
-
-
-
 # jaws.swim() # 'Jaws is swimming'
 # jaws.walk() # AttributeError: 'Aquatic' object has no attribute 'walk'
 # jaws.greet() # 'I am Jaws of the sea!'
@@ -83,5 +83,173 @@ print(f"captain_cook is instance of Ambulatory: {isinstance(captain_cook, Ambula
 # captain_cook.greet() # ??
 
 
+
+
+
+# --------------------         MRO : Method Resolution Order         --------------------
+# "Penguin" inherits from both "Aquatic" and "Ambulatory", therefore instances of Penguin can call both the walk and swim methods.
+  # What about the "greet" method for our instance of Penguin(Aquatic , Ambulatory)? It is calling the 
+    #     Aquatic.greet() 
+    #                     instead of 
+    #     Ambulatory.greet().
+
+# How is the oreder established
+
+
+# MRO:
+  # Whenever you create a class, Python sets a Method Resolution Order, or MRO, for that class, 
+  # which is the order in which Python will look for methods on instances of that class.
+
+  # It's like a Hierarchy:
+    # the rule is changed from Python 2.x to 3.x
+
+
+# There are 3 different ways to access and see the MRO for a given class
+      # __mro__ : There is __mro__ attribute on a class
+      # mro(): use the "mro()" method on the class (same result as __mro__)
+      # help(cls): RECOMENDED. Use the built in "help(cls)" method. It more human readable
+
+
+# Example: 
+Penguin.__mro__
+#	(<class 'multiple.Penguin'>, <class 'multiple.Aquatic'>,
+#	<class 'multiple.Ambulatory’>, <class 'object’>)
+
+Penguin.mro()
+#	[ __main__.Penguin, __main__.Aquatic, __main__.Ambulatory, object]
+
+help(Penguin) # best for HUMAN readability -> gives us a detailed chain
+
+
+
+# ---------    MRO: Explanation    ---------
+class A:
+  def do_something(self):
+    print("Method Defined In: A")
+
+class B(A):
+  def do_something(self):
+    print("Method Defined In: B")
+
+class C(A):
+  def do_something(self):
+    print("Method Defined In: C")
+
+class D(B,C):
+  def do_something(self):
+    print("Method Defined In: D")
+
+
+thing = D()
+thing.do_something()
+ 
+
+# Chain of the above class be like
+        #     A
+        #   /  \
+        #   B  C
+        #   \ /
+        #    D
+
+
+
+# ----------    Testing MRO    ----------
+
+class A:
+  def do_something(self):
+    print("Method Defined In: A")
+
+class B(A):
+    pass
+#   def do_something(self):
+#     print("Method Defined In: B")
+
+class C(A):
+  def do_something(self):
+    print("Method Defined In: C")
+
+class D(B,C):
+    pass
+#   def do_something(self):
+#     print("Method Defined In: D")
+
+
+thing = D()
+thing.do_something()    # Method Defined In: D
+ 
+
+# Chain of the above class be like
+        #     A
+        #   /  \
+        #   B  C
+        #   \ /
+        #    D
+
+    # MRO - hierarchy (using "mro()"):   
+        # D, B, C, A, objects
+
+
+# D is defined as below
+class D(B,C):
+    pass
+
+# then following will print: "Method Defined In: B"
+thing = D()
+thing.do_something()
+
+
+# Again if B is defined as below
+class B(A):
+    pass
+
+# then following will print: "Method Defined In: C"
+thing = D()
+thing.do_something()
+
+
+# To find out MRO of the class:
+print(D.__mro__)
+
+# following also gives same result
+print(D.mro())
+
+# (<class '__main__.D'>, <class '__main__.B'>, <class '__main__.C'>, <class '__main__.A'>, <class 'object'>)
+    # Shows us it has the hierarchy: 
+        # D
+        # B
+        # C
+        # A
+
+
+# following gives more "HUMAN READABLE" format
+help(D)
+
+""" 
+    Help on class D in module __main__:
+
+    class D(B, C)
+    |  Method resolution order:
+    |      D
+    |      B
+    |      C
+    |      A
+    |      builtins.object
+    |
+    |  Methods inherited from B:
+    |
+    |  do_something(self)
+    |
+    |  ----------------------------------------------------------------------
+    |  Data descriptors inherited from A:
+    |
+    |  __dict__
+    |      dictionary for instance variables (if defined)
+    |
+    |  __weakref__
+    |      list of weak references to the object (if defined)
+"""
+
+
 # python py_ch5_3_4_muLinhrt_MtdRsOrdr.py
 
+ 
