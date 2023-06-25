78ee57fab18fe372e6b9ad0bb6689fc402fa486b
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2023-06-25 14:22:46 +0600

    Add files via upload

---
+++ py_ch_9_iTRTR_GnRTR_DcoRTR/py_ch9_1_1_intro_iTRBL_vs_iTRT.py
@@ -0,0 +1,28 @@
+
+# Courses: colt_py_bootcamps    265, 266
+
+# --------------    iterator vs iterable    --------------
+
+# these are related to loops
+
+# Objectives: 
+    # part 1: ITEARTORS
+        # Difference between ITERATOR and ITERABLE
+            # Define Iterator and Iterable 
+
+        # User defined ITERATOR
+            # Understand the iter() and next() methods
+
+        # How make our CLASS ITERABLE
+
+        # Detail on how loops work
+            # Build our own for loop
+
+
+    # part 2: GENERATORS
+        # Define what generators are and how they can be used 
+            # How GENERATORS are related to iterators
+
+        # Compare 'generator FUNCTIONS' and 'generator EXPRESSION's 
+        # Use generators to PAUSE execution of expensive functions
+
