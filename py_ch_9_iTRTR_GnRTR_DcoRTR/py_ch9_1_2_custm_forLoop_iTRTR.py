f7a8b0b5f4006e985ad917a48b09fc62b6b8ebd7
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2023-06-28 19:34:41 +0600

    Update py_ch9_1_2_custm_forLoop_iTRTR.py

--- py_ch_9_iTRTR_GnRTR_DcoRTR/py_ch9_1_2_custm_forLoop_iTRTR.py
+++ py_ch_9_iTRTR_GnRTR_DcoRTR/py_ch9_1_2_custm_forLoop_iTRTR.py
@@ -25,4 +25,59 @@ my_for("lol", print)
 my_for([1,2,3,4,5], square)
 
 
+
+
+
+# -------------    Custom iterator    -------------
+# it is kind of reverse of defining a custom-for loop (as above)
+class Cuntr:
+	def __init__(self, low, high):
+		self.low = low
+		self.high = high
+		
+	# we have to define __iter__() Dunder to make a iterator
+	# __iter__ makes an iterable object
+	def __iter__(self):
+		return iter("hello")
+
+
+# when 'for-loop' runs on "Cuntr", it will invoke __iter__()
+
+for x in Cuntr(10, 20):
+	print(x)
+
+
+
+
+# To make above work properly, we need to return "self" from __iter__
+	# we also need to define __next__() to make it a prper iterator
+		# and raise "StopIteration" inside of it
+class Cuntr:
+	def __init__(self, low, high):
+		self.low = low
+		self.high = high
+		
+	# we have to define __iter__() Dunder to make a iterator
+	# __iter__ makes an iterable object
+	def __iter__(self):
+		return self		# this actually invokes __next__()
+
+	def __next__(self):
+		# return 1	# makes infinite loop
+		if self.low < self.high:
+			num = self.low
+			self.low += 1
+			return num
+		# we also need to raise the "StopIteration" ERR
+		raise StopIteration
+
+
+# when 'for-loop' runs on "Cuntr", it will invoke __iter__()
+
+for x in Cuntr(1, 20):
+	print(x)
+
+# "Cuntr()" actually works like "range()"
+
+
 # python py_ch9_1_2_custm_forLoop_iTRTR.py
