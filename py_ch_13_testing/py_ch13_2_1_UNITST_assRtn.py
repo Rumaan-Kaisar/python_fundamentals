2f72e3d9bc9ccc922c207fb3541402991fb6d779
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2023-07-21 14:08:36 +0600

    Update py_ch13_2_1_UNITST_assRtn.py

--- py_ch_13_testing/py_ch13_2_1_UNITST_assRtn.py
+++ py_ch_13_testing/py_ch13_2_1_UNITST_assRtn.py
@@ -54,3 +54,363 @@
     # You can run tests by calling unittest.main())
 
 
+
+
+# Example 1: lets say 'activities.py' is our app.
+            # For unittest, we have another file called "tests.py"
+
+# ********    'activities.py'    ********:
+def eat(food, ishealthy): 
+    pass
+
+def nap(num_hours): 
+    pass
+
+
+# ***********    "tests.py"    ***********:
+import unittest     
+from activities import eat, nap     #  load functionality fronm the app that we want to test
+
+# the name "ActivityTests" could be anything, but it needs to "INHERIT" from 'unittest.TestCase'
+class ActivityTests(unittest.TestCase):
+    pass
+
+# finally following runs our tests
+if __name__ == "__main__" :
+    unittest.main()
+
+
+# Using TDD method, we'll modify 'activities.py'
+    # To do that, first we need to setup unittest in "test.py"
+    # we run the "tests.py" and then modify the app to pass the failed tests
+
+
+
+# ============= ATTEMPT 1:
+# in activities.py
+# H:\shared_docs\codes_py\py_ch_13_testing\activities.py
+
+def eat(food, is_healthy):
+    pass
+
+def nap(no_og_hours):
+    pass
+
+
+
+# in tests.py
+# H:\shared_docs\codes_py\py_ch_13_testing\activities_tests.py
+import unittest
+from activities import eat, nap
+
+class ActivityTests(unittest.TestCase):
+    def test_eat(self):
+        self.assertEqual(eat("broccoli", is_healthy=True), "I'm eating broccoli, it makes my bdy a Tmpl")
+        self.assertEqual(eat("pizza", is_healthy=False), "I'm eating pizza, because YOLO!")
+
+if __name__ == "__main__": 
+    unittest.main()
+
+# try to run tests.py
+
+""" 
+H:\shared_docs\codes_py\py_ch_13_testing>python activities_tests.py
+F
+======================================================================
+FAIL: test_eat (__main__.ActivityTests)
+----------------------------------------------------------------------
+Traceback (most recent call last):
+  File "H:\shared_docs\codes_py\py_ch_13_testing\activities_tests.py", line 7, in test_eat
+    self.assertEqual(eat("broccoli", is_healthy=True), "My bdy is a Tmpl")
+AssertionError: None != 'My bdy is a Tmpl'
+
+----------------------------------------------------------------------
+Ran 1 test in 0.001s
+
+FAILED (failures=1)
+"""
+
+
+
+
+# ============= ATTEMPT 2:
+# in activities.py
+# H:\shared_docs\codes_py\py_ch_13_testing\activities.py
+
+def eat(food, is_healthy):
+    ending = "because YOLO!"
+    return f"I'm eating {food}, {ending}"
+
+def nap(no_of_hours):
+    pass
+
+
+
+# Note: We cannot have multple assertions in one test as below:
+
+# class ActivityTests(unittest.TestCase):
+#     def test_eat(self):
+#         self.assertEqual(eat("broccoli", is_healthy=True), "I'm eating broccoli, it makes my bdy a Tmpl")
+#         self.assertEqual(eat("pizza", is_healthy=False), "I'm eating pizza, because YOLO!")
+
+# EACH assertion needs its own function for testing
+
+
+
+
+# ============= ATTEMPT 3:
+# in activities.py
+# H:\shared_docs\codes_py\py_ch_13_testing\activities.py
+
+def eat(food, is_healthy):
+    ending = "because YOLO!"
+    return f"I'm eating {food}, {ending}"
+
+def nap(no_of_hours):
+    pass
+
+
+
+# in tests.py
+# H:\shared_docs\codes_py\py_ch_13_testing\activities_tests.py
+import unittest
+from activities import eat, nap
+
+class ActivityTests(unittest.TestCase):
+    def test_eat_healthy(self):
+        self.assertEqual(eat("broccoli", is_healthy=True), "I'm eating broccoli, it makes my bdy a Tmpl")
+    
+    # EACH assertion needs its own function for testing
+    def test_eat_unhealthy(self):
+        self.assertEqual(eat("pizza", is_healthy=False), "I'm eating pizza, because YOLO!")
+
+if __name__ == "__main__": 
+    unittest.main()
+
+# try to run tests.py
+# Now we've got 2 tests and 1 failure
+""" 
+F.
+======================================================================
+FAIL: test_eat_healthy (__main__.ActivityTests)
+----------------------------------------------------------------------
+Traceback (most recent call last):
+  File "H:\shared_docs\codes_py\py_ch_13_testing\activities_tests.py", line 7, in test_eat_healthy
+    self.assertEqual(eat("broccoli", is_healthy=True), "I'm eating broccoli, it makes my bdy a Tmpl")
+AssertionError: "I'm eating broccoli, because YOLO!" != "I'm eating broccoli, it makes my bdy a Tmpl"
+- I'm eating broccoli, because YOLO!
++ I'm eating broccoli, it makes my bdy a Tmpl
+
+
+----------------------------------------------------------------------
+Ran 2 tests in 0.001s
+
+FAILED (failures=1)
+"""
+
+
+
+
+# ============= ATTEMPT 4:
+# in activities.py
+# H:\shared_docs\codes_py\py_ch_13_testing\activities.py
+
+def eat(food, is_healthy):
+    ending = "because YOLO!"
+    return f"I'm eating {food}, {ending}"
+
+def nap(no_of_hours):
+    pass
+
+
+
+# in tests.py, we now add two more tests for nap()
+# H:\shared_docs\codes_py\py_ch_13_testing\activities_tests.py
+import unittest
+from activities import eat, nap
+
+class ActivityTests(unittest.TestCase):
+    def test_eat_healthy(self):
+        self.assertEqual(eat("broccoli", is_healthy=True), "I'm eating broccoli, it makes my bdy a Tmpl")
+    
+    # EACH assertion needs its own function for testing
+    def test_eat_unhealthy(self):
+        self.assertEqual(eat("pizza", is_healthy=False), "I'm eating pizza, because YOLO!")
+
+    def test_short_nap(self):
+        self.assertEqual(nap(1), "Felling Good!")
+
+    def test_long_nap(self):
+        self.assertEqual(nap(3), "Ugh I overslept!")
+
+
+if __name__ == "__main__": 
+    unittest.main()
+
+# try to run tests.py
+# Now we've got 4 tests and 3 failure
+# Notice the "F.FF", "." for pass, "F" for fail
+""" 
+H:\shared_docs\codes_py\py_ch_13_testing>python activities_tests.py
+F.FF
+======================================================================
+FAIL: test_eat_healthy (__main__.ActivityTests)
+----------------------------------------------------------------------
+Traceback (most recent call last):
+  File "H:\shared_docs\codes_py\py_ch_13_testing\activities_tests.py", line 7, in test_eat_healthy
+    self.assertEqual(eat("broccoli", is_healthy=True), "I'm eating broccoli, it makes my bdy a Tmpl")
+AssertionError: "I'm eating broccoli, because YOLO!" != "I'm eating broccoli, it makes my bdy a Tmpl"
+- I'm eating broccoli, because YOLO!
++ I'm eating broccoli, it makes my bdy a Tmpl
+
+
+======================================================================
+FAIL: test_long_nap (__main__.ActivityTests)
+----------------------------------------------------------------------
+Traceback (most recent call last):
+  File "H:\shared_docs\codes_py\py_ch_13_testing\activities_tests.py", line 17, in test_long_nap
+    self.assertEqual(nap(3), "Ugh I overslept!")
+AssertionError: None != 'Ugh I overslept!'
+
+======================================================================
+FAIL: test_short_nap (__main__.ActivityTests)
+----------------------------------------------------------------------
+Traceback (most recent call last):
+  File "H:\shared_docs\codes_py\py_ch_13_testing\activities_tests.py", line 14, in test_short_nap
+    self.assertEqual(nap(1), "Felling Good!")
+AssertionError: None != 'Felling Good!'
+
+----------------------------------------------------------------------
+Ran 4 tests in 0.001s
+
+FAILED (failures=3)
+"""
+
+
+
+
+# ============= ATTEMPT 5:
+# in activities.py
+# H:\shared_docs\codes_py\py_ch_13_testing\activities.py
+
+def eat(food, is_healthy):
+    ending = "because YOLO!"
+    # make different ending if is_healthy=True    
+    if is_healthy:
+        ending = "it makes my bdy a Tmpl"
+        
+    return f"I'm eating {food}, {ending}"
+
+def nap(no_of_hours):
+    if (no_of_hours >= 2):
+        return "Ugh I overslept!"
+    else:
+        return "Felling Good!"
+
+
+
+# in tests.py, we now add two more tests for nap()
+# H:\shared_docs\codes_py\py_ch_13_testing\activities_tests.py
+import unittest
+from activities import eat, nap
+
+class ActivityTests(unittest.TestCase):
+    def test_eat_healthy(self):
+        self.assertEqual(eat("broccoli", is_healthy=True), "I'm eating broccoli, it makes my bdy a Tmpl")
+    
+    # EACH assertion needs its own function for testing
+    def test_eat_unhealthy(self):
+        self.assertEqual(eat("pizza", is_healthy="True"), "I'm eating pizza, because YOLO!")
+
+    def test_short_nap(self):
+        self.assertEqual(nap(1), "Felling Good!")
+
+    def test_long_nap(self):
+        self.assertEqual(nap(3), "Ugh I overslept!")
+
+
+if __name__ == "__main__": 
+    unittest.main()
+
+# try to run tests.py
+# Now we've got 4 tests and 1 failure
+# Notice the ".F..", "." for pass, "F" for fail
+""" 
+H:\shared_docs\codes_py\py_ch_13_testing>python activities_tests.py
+.F..
+======================================================================
+FAIL: test_eat_unhealthy (__main__.ActivityTests)
+----------------------------------------------------------------------
+Traceback (most recent call last):
+  File "H:\shared_docs\codes_py\py_ch_13_testing\activities_tests.py", line 11, in test_eat_unhealthy
+    self.assertEqual(eat("pizza", is_healthy="True"), "I'm eating pizza, because YOLO!")
+AssertionError: "I'm eating pizza, it makes my bdy a Tmpl" != "I'm eating pizza, because YOLO!"
+- I'm eating pizza, it makes my bdy a Tmpl
++ I'm eating pizza, because YOLO!
+
+
+----------------------------------------------------------------------
+Ran 4 tests in 0.001s
+
+FAILED (failures=1)
+"""
+
+
+
+
+# ============= ATTEMPT 6 (final):
+# in activities.py
+def eat(food, is_healthy):
+    ending = "because YOLO!"
+    # make different ending if is_healthy=True    
+    if is_healthy:
+        ending = "it makes my bdy a Tmpl"
+        
+    return f"I'm eating {food}, {ending}"
+
+def nap(no_of_hours):
+    if (no_of_hours >= 2):
+        return "Ugh I overslept!"
+    else:
+        return "Felling Good!"
+
+
+
+# in tests.py
+import unittest
+from activities import eat, nap
+
+class ActivityTests(unittest.TestCase):
+    def test_eat_healthy(self):
+        self.assertEqual(eat("broccoli", is_healthy=True), "I'm eating broccoli, it makes my bdy a Tmpl")
+    
+    # EACH assertion needs its own function for testing
+    def test_eat_unhealthy(self):
+        self.assertEqual(eat("pizza", is_healthy=False), "I'm eating pizza, because YOLO!")
+
+    def test_short_nap(self):
+        self.assertEqual(nap(1), "Felling Good!")
+
+    def test_long_nap(self):
+        self.assertEqual(nap(3), "Ugh I overslept!")
+
+
+if __name__ == "__main__": 
+    unittest.main()
+
+# try to run tests.py: python activities_tests.py
+# Notice "....", all test passed
+""" 
+H:\shared_docs\codes_py\py_ch_13_testing>python activities_tests.py
+....
+----------------------------------------------------------------------
+Ran 4 tests in 0.001s
+
+OK
+"""
+
+
+
+11:22
+# -----------------    COMMENTING the Tests    -----------------
+# Previously, if all the tests passed, we've not much info, so adding comment is helpful
