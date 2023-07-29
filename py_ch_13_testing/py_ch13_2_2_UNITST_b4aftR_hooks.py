737fdf308f21da86f7efadbe426fe666e4f4eb0e
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2023-07-29 14:16:47 +0600

    Update py_ch13_2_2_UNITST_b4aftR_hooks.py

--- py_ch_13_testing/py_ch13_2_2_UNITST_b4aftR_hooks.py
+++ py_ch_13_testing/py_ch13_2_2_UNITST_b4aftR_hooks.py
@@ -28,6 +28,7 @@
             # creating instances of a class
 
     # since we're not using databases, we only focus on "setUp"
+        # It's same as setUp(), and it runs after each of the test-function
 
 
 
@@ -98,15 +99,67 @@ class Robot:
 # robot_test.py
     # following code tests the above app.
 
-    3:10    
+
+# following code tests the "robot.py" app.
+
+import unittest
+from robot import Robot
+
+class RobotTests(unittest.TestCase):
+    mega_man = Robot("Mega man", battery = 50)  # creating a Robot object
+
+    def test_charge(self):
+        mega_man = Robot("Mega man", battery = 50)  # creating a Robot object
+        mega_man.charge()   # increase charge 50 to 100
+        self.assertEqual(mega_man.battery, 100)
+
+    def test_say_name(self):
+        mega_man = Robot("Mega man", battery = 50)  # creating a Robot object again
+        self.assertEqual(mega_man.say_name(), "BEEP BOOP BEEP BOOP.  I AM MEGA MAN")    # string needs to be exact same
+        self.assertEqual(mega_man.battery, 49)      # charg was 50 when the object was created
+        
+
+if __name__ == "__main__":
+    unittest.main()
 
 
+# Notice that we have to define the Robot object multiple time.
+    # To avoid this duplication, we can use "setUp"
+
+# Note: we can define one Robot object, at the biggining of the class,
+""" 
+    class RobotTests(unittest.TestCase):
+         mega_man = Robot("Mega man", battery = 50)  # creating a Robot object
+
+        def test_charge(self):
+ """
+    # But the issue is: During test, this single object is being manipulated multiple time
+    # so using setUp is a good idea: it creates Robot object 'for each' test function
 
 
+# --------------    above code using 'setUp()'    --------------
+import unittest
+from robot import Robot
+
+class RobotTests(unittest.TestCase):
+    def setUp(self):
+        self.mega_man = Robot("Mega man", battery = 50)  # creating a Robot object
+
+    # Now we just referance the above object
+    def test_charge(self):  
+        self.mega_man.charge()
+        self.assertEqual(self.mega_man.battery, 100)
+
+    def test_say_name(self):
+        self.assertEqual(self.mega_man.say_name(), "BEEP BOOP BEEP BOOP.  I AM MEGA MAN")    # string needs to be exact same
+        self.assertEqual(self.mega_man.battery, 49)
+        
+
+if __name__ == "__main__":
+    unittest.main()
 
 
-
-
+# python robot_test.py
 
 
 
@@ -127,7 +180,7 @@ class RobotTests(unittest.TestCase):
         self.mega_man = Robot("Mega Man", battery=50)
 
     def test_charge(self):
-        self.mega_man.charge()
+        self.mega_man.charge()  # increase charge 50 to 100
         self.assertEqual(self.mega_man.battery, 100)
 
     def test_say_name(self):
@@ -140,19 +193,6 @@ class RobotTests(unittest.TestCase):
 if __name__ == "__main__":
     unittest.main()
 
-
-
-
-""" 
-    Example : Your goal in this assignment will be to add tests to the classes you created in the last section: Card  and Deck . 
-                    Try to test that instances have the right structure, and write some tests for the methods. 
-                    Use some before hooks, and try to test for expected errors as well!
-
-                    Note that the shuffle  method will be difficult to test, since it produces a random output. 
-                        Rather than trying to test randomness, you may just want to write some smaller, more straightforward tests. 
-                        For instance, you could test that shuffle  doesn't change the size of the deck, or 
-                        that the list of cards before the shuffle is in a different order than after the shuffle. 
-"""
 
 
 
