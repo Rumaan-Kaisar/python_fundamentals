7f77b59afb4356532e3334fe3cf862cb90449eba
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2023-08-04 13:43:30 +0600

    Update py_ch8_1_intro_Open_Read.py

--- py_ch_8_file_IO/py_ch8_1_intro_Open_Read.py
+++ py_ch_8_file_IO/py_ch8_1_intro_Open_Read.py
@@ -38,4 +38,61 @@ This short story is really short.
 file = open("story.txt") 
 file.read()
 
-2:57
+
+
+# -----------    FILIO in CLI    -----------
+
+>>> f = open("story.txt")
+>>> f
+<_io.TextIOWrapper name='story.txt' mode='r' encoding='cp1252'>
+
+>>> help(f)
+# Help on TextIOWrapper object:
+
+>>> print(f) # wont work, instead use read()
+
+>>> f.read()
+'This short story is really short.\n'
+# Notice "\n" new-line, because in the documant, we have an empty line 
+
+
+
+# If nothing is specified in read(), as argumnet, then "-1" will be used autometicaly
+    # "-1" will return all the content
+
+
+
+
+# ---------------    Different Modes    ---------------
+""" 
+    Character           Meaning
+        'r'	        open for reading (default)
+        'w'         open for writing, truncating the file first 
+        'x'	        open for exclusive creation, failing if the file already exists
+        'a'	        open for writing, appending to the end of the file if it exists
+
+        'b'	        binary mode
+        't'	        text mode (default)
+        '+'	        open a disk file for updating (reading and writing)
+
+        'U'	        universal newlines mode (deprecated)
+
+"""
+
+
+
+
+# ---------------    Cursor Movement    ---------------
+
+# Python reads files by using a "CURSOR"
+# This is like the cursor you see when you're TYPING
+# After a file is read, the cursor is at the END
+
+# Note that, once we read the whole file using f.read()
+
+# a "cursor" moves to the end of the file.
+    # using f.read() again will give an "Empty-String"
+    # New content shown if new content added to the "txt/file"
+        # will show ther content from the previous position of the cursor
+
+
