0dd8d2cfaac0074bb7218afab3482b9e4ef98c86
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2023-08-11 14:53:47 +0600

    Update py_ch8_5_FILIO_modes.py

--- py_ch_8_file_IO/py_ch8_5_FILIO_modes.py
+++ py_ch_8_file_IO/py_ch8_5_FILIO_modes.py
@@ -3,8 +3,23 @@
 
 
 # ---------------    FILIO : modes/flags    ---------------
+""" 
+    Modes for Opening Files
+        r   - Read a file (no writing) - this is the default 
 
+        w   - Write to a file (previous contents removed) 
 
+        a   - Append to a file (previous contents not removed) 
+                append at the end 
+                no cursor control
+                file created if doesn't exist 
+
+        r+  - Read and write to a file (writing based on cursor) 
+                OVERRITE the text from start
+                ERR if file doesn't exist
+                append from start
+                cursor control
+"""
 
 
 # w - writes and erases existing contents
@@ -13,33 +28,41 @@ with open("haiku.txt", "w") as file:
     file.write("What about the older one?\n")
     file.write("Let's go check it out")
 
+
+
 # a - appends to end, preserving original contents
-NO CONTROL OVER CURSOR
+# NO CONTROL OVER CURSOR
 with open("haiku.txt", "a") as file:
-	file.seek(0)
-	file.write(":)\n")
+    file.seek(0)    # it has no effect for "a-append" mode
+    file.write("\n This is APPENDED!!!\n")
+    file.write(":)\n")  # always append at the end
+
+
 
 # r+ read and write
+    # overrite the text from start
+    # seek/cursor works
 with open("haiku.txt", "r+") as file:
-	file.write(":)")
-	file.seek(10)
-	file.write(":(")
+    file.write("OVERRITtEn")
+    file.write(":)")
+    file.seek(10)
+    file.write(":(")
+
+# output: OVERRITtEn:(s is great
+
+
 
 # r+ will not create a file if it doesn't exist
+# with open("hello.txt", "r+") as file:      #ERR
 with open("hello.txt", "a") as file:
-	file.write("HELLO!!!")
+    file.write("HELLO There !!! How ya doin?")
 
 
 
 
-
-Copy Solution
-Copy should copy contents from one file to another.  For example, after running:
-
-copy('story.txt', 'story_copy.txt') # None
-We would expect the contents of story.txt and story_copy.txt to now be the same.
-
-Here's my solution:
+# Example 1: Copy should copy contents from one file to another.  
+            # For example, after running:   copy('story.txt', 'story_copy.txt')     # None
+            # We would expect the contents of 'story.txt' and 'story_copy.txt' to now be the same.
 
 def copy(file_name, new_file_name):
     with open(file_name) as file:
@@ -48,42 +71,65 @@ def copy(file_name, new_file_name):
     with open(new_file_name, "w") as new_file:
         new_file.write(text)
 
+copy('story.txt', 'story_copy.txt')
 
 
 
-Copy and Reverse Solution
-copy_and_reverse  is very similar to the previous exercise, copy , except that we reverse the text using a slice before we write it to the new file:
+
+# Example 2: Copy and Reverse the content. It is very similar to the previous exercise, 
+            # we reverse the text using a slice before we write it to the new file:
 
 def copy_and_reverse(file_name, new_file_name):
     with open(file_name) as file:
         text = file.read()
-
+    
     with open(new_file_name, "w") as new_file:
-        new_file.write(text[::-1])
+        new_file.write(text[::-1])  # reverse the text then write
+
+copy_and_reverse('story.txt', 'story_copy_N_rev.txt')
+# last line becomes first
+# each line is reversed
 
 
 
 
-Statistic Exercise Solution
-
-
-def statistics(file_name):
+# Example 3: Statistic of a file word, lines, characters.
+def file_statistics(file_name):
     with open(file_name) as file:
-        lines = file.readlines()
+        lines = file.readlines()    # all lines in a list
 
-    return { "lines": len(lines),
-             "words": sum(len(line.split(" ")) for line in lines),
-             "characters": sum(len(line) for line in lines) }
+    return { 
+        "lines": len(lines),
+        "words": sum(len(line.split(" ")) for line in lines),
+        "characters": sum(len(line) for line in lines) 
+        }
+
+file_statistics("story.txt")    # {'lines': 3, 'words': 13, 'characters': 69}
 
 
 
-Find and Replace Solution
+
+# Example 4: Find and Replace words of a file
 def find_and_replace(file_name, old_word, new_word):
     with open(file_name, "r+") as file:
-        text = file.read()
-        new_text = text.replace(old_word, new_word)
-        file.seek(0)
-        file.write(new_text)
-        file.truncate()
+        text = file.read()  # store the old text
+        new_text = text.replace(old_word, new_word)     # change the text
+        file.seek(0)        # cursor reset is needed because we used read()
+        file.write(new_text)    # write changed text
+        file.truncate()     # resizes the file
 
-        
+find_and_replace("hello.txt", "HELLO", "Oy")
+
+
+# ------------    Python File truncate() Method    ------------
+    # The truncate() method resizes the file to the given number of bytes.
+    # If the size is not specified, the current position will be used.
+
+""" 
+Syntax:
+        file.truncate(size)
+
+    Parameter
+        size: The size of the file (in bytes) after the truncate. Default None, which means the current file stream position.
+"""        
+
