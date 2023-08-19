4f429910436dff701e017cb79269bfe58549cd44
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2023-08-19 11:40:37 +0600

    Update py_ch14_3_pickling.py

--- py_ch_14_CSV_pikling/py_ch14_3_pickling.py
+++ py_ch_14_CSV_pikling/py_ch14_3_pickling.py
@@ -1,9 +1,26 @@
 
 # Courses: colt_py_bootcamps    320, 321
 
-# intro
+# -------------------    Pickling    -------------------
+# NOTE: it works with PYTHON
+    # dont use pickle file with other language (JAVA or C++ or JS)
+# its not related to "CSV"
+# its related to FILIO
 
+# if we have python data or python object
+# it stores the current state of data into a file
+# for example we're saving some csv data during web scrapping
+
+# we have to use pickle module
+    # it creates a pickle file
+    # Data is "serialized" into byte-stream
+    # we can then un-pickle the data whenever we want
+
+
+
+# Example 1 : Pickling demo
 import pickle
+
 class Animal:
 	def __init__(self, name, species):
 		self.name = name
@@ -29,14 +46,23 @@ class Cat(Animal):
 blue = Cat("Blue", "Scottish Fold", "String")
 
 # To pickle an object:
+    # NOTICE: the binary writing mode "wb"
 with open("pets.pickle", "wb") as file:
 	pickle.dump(blue, file)
 
 # To unpickle something:
-# with open("pets.pickle", "rb") as file:
-# 	zombie_blue = pickle.load(file)
-# 	print(zombie_blue)
-# 	print(zombie_blue.play())
+# commentout the writing pickle file
+    # NOTICE: the binary reading mode "rb"
+with open("pets.pickle", "rb") as file:
+	zombie_blue = pickle.load(file)
+	print(zombie_blue)
+	print(zombie_blue.play())
+
+
+# NOTE:we actually do not use pickle to store user data in web development
+    # it's just a quick way to store and grabbing data
+
+
 
 
 
