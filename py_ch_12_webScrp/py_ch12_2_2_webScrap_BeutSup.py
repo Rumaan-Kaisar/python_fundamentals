b403d6fdc0a1c7592631e9f96bed791c6c0673f6
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2023-08-27 11:19:47 +0600

    Update py_ch12_2_2_webScrap_BeutSup.py

--- py_ch_12_webScrp/py_ch12_2_2_webScrap_BeutSup.py
+++ py_ch_12_webScrp/py_ch12_2_2_webScrap_BeutSup.py
@@ -6,7 +6,7 @@
 # INSTALLING Beautiful soup
 # pip install bs4
 # This is a dummy package managed by the developer of Beautiful Soup to prevent name squatting. 
-    # The official name of PyPI’s Beautiful Soup Python package is "beautifulsoup4". 
+    # The official name of PyPI's Beautiful Soup Python package is "beautifulsoup4". 
     # This package ensures that if you type 'pip install bs4' by mistake you will end up with Beautiful Soup.
 
 # Instead use:
@@ -49,7 +49,7 @@
 
 
 # ---------------    Navigating with CSS Selectors    ---------------
-# select() - returns a list of elements matching a CSS selector
+# select - returns a list of elements matching a CSS selector
 
 
 
@@ -138,7 +138,7 @@ print(dt)
 
 
 # ---------------    CSS style selector    ---------------
-# use 'select()' #id for ids and ".class" for classes
+# use 'select' #id for ids and ".class" for classes
 # e.g #foo abnd .bar
 
 # --------    select    --------
@@ -189,3 +189,97 @@ soup.find_all(attrs={"data-baz": True})     # notice attribut is inside {}
 soup.select("[data-baz]")
 
 
+
+
+# ------------------    Accessing Data in Elements    ------------------
+# Extracting data from the CSS, HTML elements
+
+    # get_text() - access the inner text in an element
+    # 'name' - tag name "its not a function"
+    # 'attrs' - dictionary of attributes "its not a function"
+    # You can also access attribute values using brackets!
+
+# Lets consider following string, since the "requested HTML" sent as a giant string
+html_str_2 = """
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta class = "no_text" charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>First HTML Page</title>
+</head>
+<body>
+    <div id="first">
+        <h3 data-example="yes">hi</h3>
+        <p>more text.</p>
+    </div>
+    <ol>
+        <li class="special super-special">This list item is special.</li>
+        <li class="special">This list item is also special.</li> 
+        <li>This list item is not special.</li>
+    </ol>
+    <div data-example="yes">bye</div>
+</body>
+</html>
+"""
+
+from bs4 import BeautifulSoup
+soup3 = BeautifulSoup(html_str_2, 'html.parser')
+
+
+# ------    getText()    ------
+dt_a = soup3.select(".special")
+el = dt_a[0]
+print(el.get_text()) # 
+
+el # <li class="special">This list item is special.</li>
+
+# getting all elements
+for le in dt_a:
+    print(le.get_text())
+
+# No ERR if there is no inner-text
+print(soup3.select(".no_text")[0].getText())
+
+
+# ------    name    ------
+# getting all tag names
+for le in dt_a:
+    print(le.name)
+# li
+# li
+
+
+
+# ------    attrs    ------
+# getting all attributes
+# since there is only "class attributes" used, it will return the list of class
+    # in key-value pair
+for le in dt_a:
+    print(le.attrs)
+
+# output:
+# {'class': ['special', 'super-special']}
+# {'class': ['special']}
+
+
+# specify specific "attributes"
+for le in dt_a:
+    print(le.attrs['class'])
+
+# ['special', 'super-special']
+# ['special']
+
+
+# ------    using []    ------
+#  other way of selecting "attribute"
+attr = soup3.find("h3")["data-example"]
+print(attr)     # yes
+
+# finding "id"s inside all div
+dv_id = soup3.find("div")["id"]
+print(dv_id)     # first
+
+# getting all div attributes
+dv_attr = soup3.find("div").attrs
+print(dv_attr)     # {'id': 'first'}
