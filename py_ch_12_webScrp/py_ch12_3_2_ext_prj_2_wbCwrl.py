3af76d9d3a7e62781a8f8446d60e988d87f19d5f
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2023-09-13 18:28:40 +0600

    Update py_ch12_3_2_ext_prj_2_wbCwrl.py

--- py_ch_12_webScrp/py_ch12_3_2_ext_prj_2_wbCwrl.py
+++ py_ch_12_webScrp/py_ch12_3_2_ext_prj_2_wbCwrl.py
@@ -50,5 +50,73 @@ class BookSpider(scrapy.Spider):
             # it called on every single response it gets back, when we scrape
         # response: the thing we get from the http requests
     def parse(self, response):
-        response.css('article.product_pod')     # it is similar to soup.select(".special")
+        # response.css('article.product_pod')     # it is similar to soup.select(".special")
+        for article in response.css('article.product_pod'):
+            yield { # we have to use 'yield' (instead of return), it goes over n over
+                'price': article.css(".price_color::text")
+            }
 
+# ------------    CLI command    ------------
+""" 
+        > Scrapy 2.10.1 - no active project
+
+        Usage:
+        scrapy <command> [options] [args] 
+"""
+
+# following CLI command saves data to a csv file
+# -----------    save as CSV    -----------
+# running in CLI:   scrapy runspider -o books.csv book_scraper.py
+
+# we'll get some list of "objects" in format
+# "[<Selector query=""descendant-or-self::*[@class and contains(concat(' ', normalize-space(@class), ' '), ' price_color ')]/text()"" data='£51.77'>]"
+# We need to extract info from it:
+    # since it returns list,  we should use index
+    # an alternative wway is to use: extract_first()
+
+
+
+
+
+# -----------------    mod 2    -----------------
+
+# ----------    extract_first() & RECURSION    ----------
+# 'price': article.css(".price_color::text").extract_first()
+# 'title': article.css("h3 > a::attr(title)").extract_first()
+    # we're usong CSS-selector here
+
+# repeat it for the rest of the page: use "next" button
+    # we place following inside the FOR-loop
+        # next = response.css('.next > a::attr(href)1).extract_first()
+    # and we'l make a recursion:
+        # if next:
+        #         yield response.follow(next, self.parse)
+
+
+import scrapy
+
+# inheriting from scrapy.Spider
+class BookSpider(scrapy.Spider):
+    name = "bookspider"
+    # list of urls we want to scrap
+    start_urls = ['https://books.toscrape.com/']
+
+    # parse: it is required for every 'spider'
+        # it parse the result of the request, it invokes "requests" autometically
+        # parse is called over & over depend on how we define 'parse':
+            # it called on every single response it gets back, when we scrape
+        # response: the thing we get from the http requests
+    def parse(self, response):
+        # response.css('article.product_pod')     # it is similar to soup.select(".special")
+        for article in response.css('article.product_pod'):
+            yield { # we have to use 'yield' (instead of return), it goes over n over
+                'price': article.css(".price_color::text").extract_first(),
+                'title': article.css("h3 > a::attr(title)").extract_first()
+            }
+            next = response.css('.next > a::attr(href)').extract_first()
+            if next:
+                yield response.follow(next, self.parse)
+
+# we can save the data into a JSON file too
+# -----------    save as JSON    -----------
+# scrapy runspider -o books3.json book_scraper.py
