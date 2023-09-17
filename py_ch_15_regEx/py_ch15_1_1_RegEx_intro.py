a342fb39d0e1a1220eb3c9fbc5e97fa0bf693a70
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2023-09-17 15:32:38 +0600

    Update py_ch15_1_1_RegEx_intro.py

--- py_ch_15_regEx/py_ch15_1_1_RegEx_intro.py
+++ py_ch_15_regEx/py_ch15_1_1_RegEx_intro.py
@@ -60,5 +60,50 @@ def hello_world(thingy):
 
 
 # --------------    RegEx outside of python    --------------
+# There is a browser tool to test any RegEx as "Live"
+    # pythex: It's a pyython specific RegEx tester
+
+# There are a ton of REGEX SYMBOLS.
+    # we're just going to cover some important ones
+    # dont need to know all symbols, just cover most usefull ones and use StackOverlow
+    # use RegEx cheatsheet/online e.g. : Rex Egg
+
+
+
+# pythex: It's a pyython specific RegEx tester
+        # Copy and paste any text-strings
+        # type regular characters: a, b, c
+        # use \ as escape charcters
+        # '.' means select-all, use '\.' to select '.'
+
+
+# Group 1 : RegEx Syntax
+""" 
+        \d  digit 0-9: 
+                        \d selects 9, 99, 123; each digits seperately
+                        \d\d selects all 2-digit nubers: 12, 99
+                        \d \d: selects numbers with space between them; 3 3, 8 4
+        
+        \w  letter, digit, or underscore (word-characters)
+                        \w \w: selects letters & space but no newline or tabs
+                        \w\s\w: selects all lettes with 'space in between'
+
+        \s  whitespace character: Tabs, Newlines
+
+        \D  not a digit
+
+        \W  not a word character:
+                        \W: selcts all non-word characters: spaces , . 
+
+        \S  not a whitespace character
+
+        .   any character except line break
+
+        Notice: uppercase is used for 'negation'
+"""
+
+# Select all 5-letter words:
+    # " \w\w\w\w\w": didnt catch all, instead use
+    # "\s\w\w\w\w\w\s"
 
 
