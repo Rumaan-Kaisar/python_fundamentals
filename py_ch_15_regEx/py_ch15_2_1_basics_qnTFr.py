7289be15e4a803ce3f675374b645e1a7a0d6b016
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2023-09-17 15:34:00 +0600

    Update py_ch15_2_1_basics_qnTFr.py

--- py_ch_15_regEx/py_ch15_2_1_basics_qnTFr.py
+++ py_ch_15_regEx/py_ch15_2_1_basics_qnTFr.py
@@ -1,1 +1,27 @@
-# Courses: colt_py_bootcamps    86, 87
+# Courses: colt_py_bootcamps    341
+
+# ----------------    Quantifers    ----------------
+# Group 2 : RegEx Syntax
+""" 
+        +       One or more
+        {3}     Exactly x times. {3}-3 times
+        {3,5}   Three to five times
+        {4,}    Four or more times
+        *       Zero or more times
+        ?       Once or none (optional)
+"""
+# these specifies how many times something should ocuurs 'in a pattern'
+
+# ------------------ test string --------
+hello world aaabbbcccdddeeef ffggg 
+I like cats and kittens :)
+PURPLE
+kitty@gmail.com
+She is 49 years old. I am 75 years old. He is 3 3.
+# ---------------------------------------
+
+# -----    +  : one or more    -----
+# Select "Any word start with k"
+# use: 'k\w+', (using only 'k\w' only selects 2-letters). 'ke' is selected from 'like'
+    # better : '\sk\w+' selects words only starts with 'k'
+
