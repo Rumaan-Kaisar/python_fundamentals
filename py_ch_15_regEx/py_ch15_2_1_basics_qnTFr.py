b6afa7a358362c45fdcf70d1d7665b883688530a
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2023-09-19 13:01:40 +0600

    Update py_ch15_2_1_basics_qnTFr.py

--- py_ch_15_regEx/py_ch15_2_1_basics_qnTFr.py
+++ py_ch_15_regEx/py_ch15_2_1_basics_qnTFr.py
@@ -1,3 +1,4 @@
+
 # Courses: colt_py_bootcamps    341
 
 # ----------------    Quantifers    ----------------
@@ -14,14 +15,52 @@
 
 # ------------------ test string --------
 hello world aaabbbcccdddeeef ffggg 
-I like cats and kittens :)
-PURPLE
-kitty@gmail.com
+I like cats and kittens :)  415 4129876
+PURPLE 415 412-9876
+kitten@gmail.com
+310 467-9876
 She is 49 years old. I am 75 years old. He is 3 3.
 # ---------------------------------------
 
 # -----    +  : one or more    -----
 # Select "Any word start with k"
-# use: 'k\w+', (using only 'k\w' only selects 2-letters). 'ke' is selected from 'like'
+    # use: 'k\w+', (using only 'k\w' only selects 2-letters). 'ke' is selected from 'like'
     # better : '\sk\w+' selects words only starts with 'k'
+
+
+
+
+# -----    {x} Exactly x times    -----
+    # select a 5-letter long word 
+    # '\s\w\w\w\w\w' or "\s\w{9}"
+
+# matching digits of pattern '*** ***-****'
+    # '\d{3} \d{3}-\d{4}'
+
+
+
+# -----    range the occurance times: {3,5}   Three to five times    -----
+    # select word characters between 5 to 7 characters 
+    # "\w{5,7}"
+
+
+
+# -----    lowwer bound: {4,}    -----
+    # select all numbers having 3 or more digits:
+    # "\d{3,}"
+
+
+
+# -----    * : Zero or more times    -----
+    # following selects: 'ac', 'abc', 'abbc', 'abbbc', 'abbbbc', ... etc
+    # "ab*c"
+
+
+
+# -----    ? : Once or none (optional)    -----
+    # following selects 0 or 1 's': 'kitten' and 'kittens' but no 'kittenss'
+    # "kittens?"
+
+    # in following case '-' becomes optional
+    # "'\d{3} \d{3}-?\d{4}'"
 
