49b1e184032a178e9371dd98e9c1226d7977b14a
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2023-09-22 15:04:07 +0600

    Update py_ch15_2_3_capch_group.py

--- py_ch_15_regEx/py_ch15_2_3_capch_group.py
+++ py_ch_15_regEx/py_ch15_2_3_capch_group.py
@@ -1,1 +1,47 @@
+
+# Courses: colt_py_bootcamps    344
+
+
+# -----------------    logical OR - |    -----------------
+# Your test string:
+# 415 345 0998 
+# (415) 345 0998
+
+# '\d{3} \d{3} \d{4}' only mathes the first string
+
+
+# matching the paranthesis '()'
+    # to mathc the '(415)' use:     '\(\d{3}\)'
+
+# Selecting both string using OR '|'
+    # In this case the OR is applied to whole-part "\d{3} \d{3} \d{4}":   '\(\d{3}\)|\d{3} \d{3} \d{4}'
+    # To solve this we use "()" withot '\':  '(\(\d{3}\)|\d{3}) \d{3} \d{4}'
+
+
+
+
+# ---------     more on PARAMS: Grouping     ---------
+    # matching "Mr. Mrs. or Miss"
+
+# Your test string:
+# Mr. Luca Guadagnino 
+# Mrs. Tilda Swinton
+
+# Naieve Way:
+'(Mr\.|Mrs\.) [A-Za-z]+[A-Za-z]+'
+
+# we can however group these as below:
+'(Mr\.|Mrs\.) ([A-Za-z]+[A-Za-z]+)'
+# the benifit is: in Python the result will comes in two parts (as group)
+
+# following has 3 groups: "Mr/Mrs", "First-name", "Last-name"
+'(Mr\.|Mrs\.) ([A-Za-z]+) ([A-Za-z]+)'
+# Notice the '+' are inside the '()'
+# Makes esier to save those into DataBases
+# to match URLS
+
+
+
+# 8:10
+
 # Courses: colt_py_bootcamps    86, 87
