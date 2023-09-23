80d3595f54618b203de3d0321091e1216f54b532
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2023-09-23 12:39:22 +0600

    Update py_ch15_3_1_RE_mdul.py

--- py_ch_15_regEx/py_ch15_3_1_RE_mdul.py
+++ py_ch_15_regEx/py_ch15_3_1_RE_mdul.py
@@ -1,1 +1,41 @@
-# Courses: colt_py_bootcamps    86, 87
+
+# Courses: colt_py_bootcamps    345
+
+# ---------------    RE: RegEx in python    ---------------
+# usage of RegEx in Python:
+    # match a string
+    # define a function that returns true if something matches a pattern
+    # define a function that 'extracts' all 'matches' from a given string and returns them in a list
+
+# you should learn the useful-functions from the 're' module documnetation
+    # compile
+    # search
+    # match
+    # split
+    # findall
+    # sub
+
+# What we'll do:
+    # defining a regular expression as a string 
+    # compiling it into a regular expression object and 
+    # using that to find matches in another string.
+
+
+# -------------    using RegEx in python    -------------
+# import regex module 
+import re
+
+# define our phone number regex, (make an object using compile() )
+pattern = re.compile(r'\d{3} \d{3}-\d{4}')  
+# note: 
+    # re.compile() creates a RegEx object that contain useful RegEx methods
+    # 'r' stands for raw-string
+
+# search a string with our regex (using search() )
+result = pattern.search('Call me at 415 555-4242!')
+    # it gives us a "match-object"
+
+
+# 3:37
+
+
