422931235752457e6602861bd489985a4cc151a9
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2023-09-27 14:29:17 +0600

    Update py_ch15_3_2_RegEx_prj_phn.py

--- py_ch_15_regEx/py_ch15_3_2_RegEx_prj_phn.py
+++ py_ch_15_regEx/py_ch15_3_2_RegEx_prj_phn.py
@@ -39,8 +39,69 @@ print(extract_all_phones("my number is 432 56"))
 # -----    Attempt : 2    -----
 # 'def is_valid_phone' will checks if th enumber is valid phone number
 
-# 5:40
+# validating inputs: useful for valdating emails, phone-nuumbers, credit-card no, and more.
 
+is_valid("something mathed")    # True
+is_valid("Not mathed")    # False
+
+# we can accomplish this by using '^' and "$" at start and end of the pattern
+    # ^ : means start with      $ : means end with
+import re
+def is_valid_phone(input):
+    phn_rgEx = re.compile(r'^\d{3} \d{3}-\d{4}$')
+    match = phn_rgEx.findall(input)
+    if match:
+        return True
+    return False
+
+print(is_valid_phone("432 567-8976"))
+print(is_valid_phone("432 567-8976 ads"))
+print(is_valid_phone("asd 432 567-8976 d"))
+
+
+
+# ------    fullmatch    ------
+# instead of using '^' and '$' we can use fullmatch()
+import re
+def is_valid_phone(input):
+    phn_rgEx = re.compile(r'\d{3} \d{3}-\d{4}')
+    match = phn_rgEx.fullmatch(input)
+    if match:
+        return True
+    return False
+
+print(is_valid_phone("432 567-8976"))
+print(is_valid_phone("432 567-8976 ads"))
+print(is_valid_phone("asd 432 567-8976 d"))
+
+# If we use same pattern in different functions, 
+    # then it's better to compile the pattern outside all the function
+
+
+
+
+""" 
+    Example 1: Time Validating
+                The time must start with a digit, and there can be a second optional digit before the colon.  
+                Then there's the colon and two mandatory digits.  
+                    I used ^ and $ to make sure the time was the only thing in the input string.
+                    The regular expression I used is: 
+
+                        ^\d\d?:\d\d$
+
+"""
+
+import re 
+
+def is_valid_time(input):
+    pattern = re.compile(r'^\d\d?:\d\d$')
+    match = pattern.search(input)
+    if match:
+        return True
+    return False
+
+print(is_valid_time("42:98"))
+print(is_valid_time("12:60ads"))
 
 
 
@@ -90,7 +151,5 @@ print(extract_all_phones("my number is 432 56"))
 print(is_valid_phone("432 567-8976"))
 print(is_valid_phone("432 567-8976 ads"))
 print(is_valid_phone("asd 432 567-8976 d"))
-
-
 
 
