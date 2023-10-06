cceb70219bd3c8c1d5d741d27d23186e726b8184
Rumaan S. Kaisar <75844616+Rumaan-Kaisar@users.noreply.github.com>
2023-10-06 09:23:45 +0600

    Update py_ch15_4_3_substut.py

--- py_ch_15_regEx/py_ch15_4_3_substut.py
+++ py_ch_15_regEx/py_ch15_4_3_substut.py
@@ -42,3 +42,71 @@ print(pattern.search(text).groups())    # find first match and show in group-tou
 # using sub() to REPLACE the matches
 result= pattern.sub('REDACTED', text) 
 print(result)
+
+
+
+
+
+# -----------    usage of CAPTURE GROUP in sub()    -----------
+    # point a group using 'number' or 'name'
+
+
+# Example 2: Removing names and replace them in their first letter
+                # e.g. Mrs. Daisy to Mrs. D. and Mr. White to Mr. W.
+import re 
+text = "Last night Mrs. Daisy and Mr. White murdered Ms. Chow"
+pattern = re.compile(r"(Mr\.|Mrs\.|Ms\.) ([A-Za-z]+)", re.I)
+
+# using sub() to REPLACE the matches
+    # point to the group (Mr\.|Mrs\.|Ms\.) using "number"
+result= pattern.sub('\g<1> REDACTED', text) # prints 'Mr\Mrs\Ms' + 'REDACTED'
+print(result)   # Last night Mrs. REDACTED and Mr. REDACTED murdered Ms. REDACTED
+
+# it's matching whatever the first group was
+result= pattern.sub('REDACTED \g<1>', text) # prints 'Mr\Mrs\Ms' + 'REDACTED'
+print(result)   # Last night REDACTED Mrs. and REDACTED Mr. murdered REDACTED Ms
+
+# no group 
+result= pattern.sub('REDACTED', text) 
+print(result)   # Last night REDACTED and REDACTED murdered REDACTED
+
+
+# To 'capture the first letter' we can modify our pattern 
+pattern = re.compile(r'(Mr.|Mrs.|Ms.) ([a-z])[a-z]+', re.I)
+# the second group captures the first letter only, notice no '+' is used
+result = pattern.sub("\g<1> \g<2>", text)
+print(result)
+
+
+
+
+# -------------    instructor codes    -------------
+import re
+text = "Last night Mrs. Daisy and Mr. white murdered Ms. Chow"
+
+pattern = re.compile(r'(Mr.|Mrs.|Ms.) ([a-z])[a-z]+', re.I)
+result = pattern.sub("\g<1> \g<2>", text)
+print(result)
+
+
+
+
+# Example 3: Profanity Filter
+                # My regex is pretty simple:
+
+                #           \bfrack\w*\b
+
+                # It looks for a word boundary and then the letters "frack" and then optionally more word characters afterwards, 
+                    # and then a word boundary again. 
+
+                # I used the word boundaries to prevent false positives if the "frack" occurs in the middle of another words
+
+                # Here's the complete solution.  Notice I used the re.IGNORECASE flag:
+
+import re
+
+def censor(input):
+    pattern = re.compile(r'\bfrack\w*\b', re.IGNORECASE)
+    return pattern.sub("CENSORED", input)
+
+print(censor("What the frack are you talkin about? I'm frakin exusted!! You're frakin dead... Frack! FRACK!!"))
